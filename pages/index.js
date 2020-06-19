40966a2b26ad4626bc222e90f4db2d46f74cbfe4
vinodpatidar123 <pvinod813@gmail.com>
2020-06-19 11:30:11 +0530

    added api routes

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,20 @@ import Head from 'next/head'
 import Link from "next/link"
 import Date from '../components/date'
 import utilStyles from "../styles/utils.module.css"
-import { getSortedPostsData } from '../libs/posts'
+import fetch from "node-fetch";
 import Layout, {siteTitle} from '../components/layout'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  console.log(allPostsData);
+export const getStaticProps = async()=> {
+  const res = await fetch("http://localhost:3001/posts");
+  const allPostsData =  await res.json();
   return {
     props: {
       allPostsData
     }
   }
 }
+
+
 
 export default function Home({allPostsData}) {
   return (
@@ -26,8 +28,10 @@ export default function Home({allPostsData}) {
   </section>
   <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
     <h2 className={utilStyles.headingLg}>Blog</h2>
-    <ul className={utilStyles.list}>
+    <div className={utilStyles.container}>
+      {/* <ul className={utilStyles.list}> */}
       {allPostsData.map(({ id, date, title }) => (
+        <div className={utilStyles.col}>
         <li className={utilStyles.listItem} key={id}>
         <Link href="/posts/[id]" as={`/posts/${id}`}>
           <a>{title}</a>
@@ -37,9 +41,15 @@ export default function Home({allPostsData}) {
           <Date dateString={date} />
         </small>
       </li>
+        </div>
       ))}
-    </ul>
+    {/* </ul> */}
+    </div>
   </section>
+  <button style={{
+    padding : "10px",
+    backgroundColor : ""
+  }} >Add Blog</button>
 </Layout>
   )
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../libs/posts'
+import fetch from "node-fetch";
 import Head from "next/head"
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
@@ -15,21 +16,35 @@ export default function Post({postData}) {
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div>
+          <p>
+            {postData.content}
+          </p>
+        </div>
       </article>
   </Layout>
   )
 }
 
 export async function getStaticPaths() {
-    const paths = getAllPostIds()
+    const res = await fetch(`http://localhost:3001/posts`);
+    const data = await res.json()
+    let paths = data.map(p =>{
+      return {
+        params :{
+          id : p.id
+        }
+      }
+    })
     return {
       paths,
       fallback: false
     }
   }
   export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id)
+    const res = await fetch(`http://localhost:3001/course/course/${params.id}`);
+    const postData = await res.json();
+    console.log(postData);
     return {
       props: {
         postData
